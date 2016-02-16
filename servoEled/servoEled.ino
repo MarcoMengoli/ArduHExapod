4924ac19d7cd14c3363d7ef5dab2527c8aec4c2d
Marco Mengoli <marco.mengoli4@gmail.com>
2016-02-16 18:42:59 +0100

    eliminated old reference to a potentiometer usage

--- servoEled/servoEled.ino
+++ servoEled/servoEled.ino
@@ -4,10 +4,10 @@
 const int servoPin = 8;
 const int servoSpeed = 5;
 
-Servo servomotore;
+Servo ser;
 LiquidCrystal lcd(12,11,5,4,3,2);
 
-int potVal;
+int val;
 int angle;
 boolean ascending = true;
 int r = 0;
@@ -17,51 +17,45 @@ void setup()
     Serial.begin(9600);
     lcd.begin(16,2);
     
-    lcd.print("Viva mengo");
+    lcd.print("Row1");
     lcd.setCursor(0,1);
-    lcd.print("viva noi");
+    lcd.print("Row2");
     
-    servomotore.attach(servoPin);
+    ser.attach(servoPin);
 }
 
 void loop()
 {
-  // potVal = analogRead(potenziometroPin);
-  
-//  Serial.print("Potenziometro valore: ");
-//  Serial.print(potVal);
-//  Serial.print("V");
-  
   if(ascending){
-     potVal += servoSpeed;
+    val += servoSpeed;
   }
   else{
-    potVal -= servoSpeed;
+    val -= servoSpeed;
   }
   
-  if(potVal >= 900)
+  if(val >= 900)
   {
-    potVal = 900;
+    val = 900;
     ascending = false; 
   }
-  else if (potVal <= 0)
+  else if (val <= 0)
   {
-    potVal = 0;
+    val = 0;
     ascending = true;
   }
   
-  angle = map(potVal, 0, 1023, 0, 179);
+  angle = map(val, 0, 1023, 0, 179);
   
   if(r++ == 20)
   {
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("Angolo: ");
+    lcd.print("Angle: ");
     lcd.setCursor(8,0);
     lcd.print(angle);
     r = 0;
   }
   
-  servomotore.write(angle);
+  ser.write(angle);
   delay(15);
 }
